2bd915d63455a872e5de207d8c92c2ab33bcf3f6
rouchie <rouchie@163.com>
2020-06-20 18:23:10 +0800

    009

---
+++ BV1UJ41197eU/009/009.pro
@@ -0,0 +1,7 @@
+
+QT += widgets core gui
+
+CONFIG += c++11
+
+SOURCES += \
+    main.cpp
---
+++ BV1UJ41197eU/009/main.cpp
@@ -0,0 +1,35 @@
+#include <QApplication>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+
+int main(int ac, char **av)
+{
+    QApplication app(ac, av);
+
+    QWidget *window = new QWidget;
+    window->setWindowTitle("My App");
+
+    QGridLayout *layout = new QGridLayout;
+
+    QLabel *label1 = new QLabel("Name: ");
+    QLineEdit *txtName = new QLineEdit;
+
+    QLabel *label2 = new QLabel("Name: ");
+    QLineEdit *txtName2 = new QLineEdit;
+
+    layout->addWidget(label1, 0, 0);
+    layout->addWidget(txtName, 0, 1);
+
+    layout->addWidget(label2, 1, 0);
+    layout->addWidget(txtName2, 1, 1);
+
+    QPushButton *button = new QPushButton("OK");
+    layout->addWidget(button, 2, 0, 1, 2);
+
+    window->setLayout(layout);
+    window->show();
+
+    return app.exec();
+}

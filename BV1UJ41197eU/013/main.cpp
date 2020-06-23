8064e141a7d1a0140e480512ce66f07cc06c7ca3
rouchie <rouchie@163.com>
2020-06-23 22:16:52 +0800

    013

---
+++ BV1UJ41197eU/013/main.cpp
@@ -0,0 +1,46 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+
+void Write(QString Filename)
+{
+    QFile mFile(Filename);
+
+    if (!mFile.open(QFile::WriteOnly | QFile::Text)) {
+        qDebug() << "could not open file for write";
+        return ;
+    }
+
+    QTextStream out(&mFile);
+    out << "Hello, bilibili";
+
+    mFile.flush();
+    mFile.close();
+}
+
+void Read(QString Filename)
+{
+    QFile mFile(Filename);
+
+    if (!mFile.open(QFile::ReadOnly | QFile::Text)) {
+        qDebug() << "could not open file for read";
+        return ;
+    }
+
+    QTextStream in(&mFile);
+    QString  s = in.readAll();
+
+    qDebug() << s;
+
+    mFile.close();
+}
+
+int main(int ac, char **av)
+{
+    QCoreApplication app(ac, av);
+
+    Read(":/013.pro");
+
+    return app.exec();
+}
